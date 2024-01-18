c223f64bd15ca0d4e36cbada2837c0d871568238
Francisco Oliva <folivacontreas@gmail.com>
2024-01-18 15:01:19 -0600

    feature: include path configurations
    Create paths to resolve to services in kubernetes
    only include cloud profile

--- src/main/java/com/study/docker/GatewayServerApplication.java
+++ src/main/java/com/study/docker/GatewayServerApplication.java
@@ -9,14 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
-        testMethods();
         SpringApplication.run(GatewayServerApplication.class, args);
-    }
-
-    public static void testMethods() {
-        System.out.println("ENTRANDO A UN METODO DE PRUEBA");
-        System.out.println("PARA VALIDAR SI SE CREA EL TAG DE FORMA AUTOMATICA AL HACER PUSH A DEVELOP");
-        System.out.println("VLV");
     }
 
 }
