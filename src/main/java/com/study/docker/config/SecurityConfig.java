7f97f86b3eb04462d4de64208a8c8bbb15f8451d
Francisco Oliva <folivacontreas@gmail.com>
2024-01-21 01:35:22 -0600

    disable jwt auth

--- src/main/java/com/study/docker/config/SecurityConfig.java
+++ src/main/java/com/study/docker/config/SecurityConfig.java
@@ -8,30 +8,30 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    SecurityFilterChain securityFilter(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
-                // Indicamos los paths que deben de tener autenticacion y el rol asociado
+//    @Bean
+//    SecurityFilterChain securityFilter(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity
+//                .csrf(AbstractHttpConfigurer::disable)
+//                // Indicamos los paths que deben de tener autenticacion y el rol asociado
+////                .authorizeHttpRequests(http -> http
+////                        .requestMatchers("/store/**").hasAuthority("admin_client_role")
+////                        .requestMatchers("/goods/**").hasAuthority("user_client_role")
+////                        .anyRequest().authenticated())
+//
+//                // Se indica que todos los request deben estar autenticados y los que no necesitan autenticacion
 //                .authorizeHttpRequests(http -> http
-//                        .requestMatchers("/store/**").hasAuthority("admin_client_role")
-//                        .requestMatchers("/goods/**").hasAuthority("user_client_role")
+//                        .requestMatchers("/app/sign").permitAll()
 //                        .anyRequest().authenticated())
-
-                // Se indica que todos los request deben estar autenticados y los que no necesitan autenticacion
-                .authorizeHttpRequests(http -> http
-                        .requestMatchers("/app/sign").permitAll()
-                        .anyRequest().authenticated())
-
-                // Se indica que se configure la conexion con el servidor de recursos para realizar la validacion de tokens, no se añade informacion adicional:
-                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> {}))
-
-                // Se indica que no se almacenara informacion de las sesiones (usado para servidores de autenticacion y autorizacion)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
-    }
+//
+//                // Se indica que se configure la conexion con el servidor de recursos para realizar la validacion de tokens, no se añade informacion adicional:
+//                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> {}))
+//
+//                // Se indica que no se almacenara informacion de las sesiones (usado para servidores de autenticacion y autorizacion)
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .build();
+//    }
 }
